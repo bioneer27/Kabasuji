73288a243242fd5557430469446d9a68aefd84e4
xavier <xavier@laptop>
2016-04-16 13:34:03 -0400

    Added Square class

---
+++ src/Kabasuji/Square.java
@@ -0,0 +1,32 @@
+package Kabasuji;
+
+import java.awt.Color;
+
+public class Square {
+	int row;
+	int col;
+	Color color;
+	enum type;
+	int setNumber;
+	boolean visible;
+	
+	public Square(int row, int col, Color color, enum type, int setNumber, boolean visible){
+		this.row = row;
+		this.col = col;
+		this.color = color;
+		this.type = type;
+		this.setNumber = setNumber;
+		this.visible = visible;
+	}
+	
+	public void snap(int snapRow, int snapCol){
+		return;
+	}
+	
+	public boolean isCovered(){
+		return false;
+	}
+	
+	
+
+}
