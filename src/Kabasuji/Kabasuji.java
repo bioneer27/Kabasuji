b9215ff2c45ef91ec0d1381a75b5fc3ccb5551c5
xavier <xavier@laptop>
2016-04-16 14:12:15 -0400

    Added Kabasuji Class

---
+++ src/Kabasuji/Kabasuji.java
@@ -0,0 +1,20 @@
+package Kabasuji;
+
+public class Kabasuji {
+	Level[] levels;
+	Badge[] badges;
+	
+	public Kabasuji(){
+		/*Add something here*/
+	}
+
+	public void resetBadges(){
+		/*Add logic here*/
+		return;
+	}
+	
+	public void playLevel(PieceType type, int lvl){
+		/*Add logic here*/
+		return;
+	}
+}
