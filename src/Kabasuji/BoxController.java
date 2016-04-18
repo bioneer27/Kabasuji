423418ba5c3c6863b2d89264d2c8cc88129a0640
xavier <xavier@laptop>
2016-04-17 21:01:47 -0400

    Added some view classes

--- src/Kabasuji/BoxController.java
+++ src/Kabasuji/BoxController.java
@@ -1,9 +1,22 @@
 package Kabasuji;
 
-public class BoxController {
+import java.awt.event.MouseEvent;
+
+
+public class BoxController extends java.awt.event.MouseAdapter{
 	
 	Box b;
 	BoxView bv;
+	
+	
+	public void mouseClicked(MouseEvent me){
+		return;
+	}
+	
+	public void mouseReleased(MouseEvent me){
+		return;
+	}
+	
 	
 	
 
