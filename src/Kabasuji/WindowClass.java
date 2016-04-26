b90800d1a0a6b75866c289653353dd78c7f60a7e
xavier <xavier@laptop>
2016-04-26 15:41:15 -0400

    Rotate works, need to get rid of the old draw however

--- src/Kabasuji/WindowClass.java
+++ src/Kabasuji/WindowClass.java
@@ -321,14 +321,14 @@ public class WindowClass extends Canvas implements MouseListener, MouseMotionLis
 	private void rotatePiece(){
 		
 		for (SquareCopy s: squareList){
-			s.rotateHelper(-3);							
+			s.rotateHelper(-96);							
 		}
 
 		for (SquareCopy s: squareList){
 			s.rotateAroundOrigin();							
 		}
 		for (SquareCopy s: squareList){
-			s.rotateHelper(3);							
+			s.rotateHelper(96);							
 		}
 		
 	}
