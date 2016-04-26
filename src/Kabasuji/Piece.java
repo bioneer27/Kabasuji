e05854b31b02d13908626f5691c8e762c476f4a7
himanjal <himanjalsharma1@gmail.com>
2016-04-26 18:43:16 -0400

    piece

--- src/Kabasuji/Piece.java
+++ src/Kabasuji/Piece.java
@@ -79,8 +79,25 @@ public class Piece {
 			}
 		}
 	}
-	*/
 	
+	public void rotateRight(){
+		Square[][] temp = new Square[6][6];
+		for(int i =0;i<6;i++){
+			for(int j=0; j<6;j++){
+				temp[i][j] = squares[i][j];
+			}
+		}
+		
+		for(int i=1; i<6; i++){
+			for(int j =1; j<6; j++){
+				
+			int row = temp[i][j].row;
+			int col = temp[j][j].col;
+			squares[i][j] = temp[col][5-row];
+			}
+		}
+	}
+	*/
 	public void setPiece(PieceType type){
 		return;
 	}
