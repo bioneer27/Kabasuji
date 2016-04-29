587a47476391e564b9ca209f93be4ba686c5621c
Alex Guerra <silverx@verizon.net>
2016-04-29 14:34:50 -0400

    builder controller shell

--- src/builderController/LevelBuilderMenuController.java
+++ src/builderController/LevelBuilderMenuController.java
@@ -1,5 +1,26 @@
 package builderController;
 
-public class LevelBuilderMenuController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import builderModel.BuilderModel;
+import builderView.LevelBuilderMenu;
+import builderView.LevelBuilderView;
+
+public class LevelBuilderMenuController implements ActionListener{
+	//Views this view can get to
+	LevelBuilderView lvlbView;
+	
+	//copy of this boundary to dispose
+	LevelBuilderMenu bmenuView;
+	
+	//do we need a model?
+	BuilderModel bmodel;
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
