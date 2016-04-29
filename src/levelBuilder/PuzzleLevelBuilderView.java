2cac60de46c9293ce04fef3219603210e26efc65
Alex Guerra <silverx@verizon.net>
2016-04-29 14:23:18 -0400

    packages for bulder

--- src/levelBuilder/PuzzleLevelBuilderView.java
+++ src/levelBuilder/PuzzleLevelBuilderView.java
@@ -10,6 +10,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import builderView.LevelBuilderMenu;
 import view.PlayMenuView;
 
 import javax.swing.GroupLayout;
