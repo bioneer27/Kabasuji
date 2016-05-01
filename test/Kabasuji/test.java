a3253dc817c45cb67197a62cca1e4200a0f7c192
Jetro <spirasmepulkul@wpi.edu>
2016-05-01 12:59:39 -0400

    added some stuff to test

--- test/Kabasuji/test.java
+++ test/Kabasuji/test.java
@@ -48,33 +48,45 @@ public class test extends TestCase {
 
 	public void testMainMenuView(){
 		
-		MainMenuView mainMenu = new MainMenuView(this.kabasuji);
-		mainMenu.setVisible(true);
-		AchievementView achView = new AchievementView(this.kabasuji);
-		mainMenu.getAchievements().doClick();
-		achView.getBack().doClick();
-		mainMenu.getRules().doClick();
-		RuleView rView = new RuleView(this.kabasuji);
-		rView.getBack().doClick();
-		mainMenu.getCredits().doClick();
-		CreditView cView = new CreditView(this.kabasuji);
-		cView.getBack().doClick();
-		//MainMenuController mainMenuController = new MainMenuController(mainMenu, this.kabasuji);
-		mainMenu.getPlay().doClick();
-		PlayMenuView playMenu = new PlayMenuView(this.kabasuji);
-		playMenu.getPuzzle().doClick();
-		AllLevelsView allLevelView = new AllLevelsView(this.kabasuji, PieceType.PUZZLE);
-		allLevelView.getLevel1().doClick();
-		LevelView lv = new LevelView(this.kabasuji, this.kabasuji.getLevel(PieceType.PUZZLE, 1));
-		lv.getBack().doClick();
-		allLevelView.getLevel2().doClick();
-		LevelView lv2 = new LevelView(this.kabasuji, this.kabasuji.getLevel(PieceType.PUZZLE, 2));
-		lv2.getBack().doClick();
-		allLevelView.getBack().doClick();
-		AllLevelsView allLevelView2 = new AllLevelsView(this.kabasuji, PieceType.LIGHTNING);
-		allLevelView2.getLevel1().doClick();
+//		MainMenuView mainMenu = new MainMenuView(this.kabasuji);
+//		mainMenu.setVisible(true);
+//		AchievementView achView = new AchievementView(this.kabasuji);
+//		mainMenu.getAchievements().doClick();
+//		achView.getBack().doClick();
+//		mainMenu.getRules().doClick();
+//		RuleView rView = new RuleView(this.kabasuji);
+//		rView.getBack().doClick();
+//		mainMenu.getCredits().doClick();
+//		CreditView cView = new CreditView(this.kabasuji);
+//		cView.getBack().doClick();
+//		//MainMenuController mainMenuController = new MainMenuController(mainMenu, this.kabasuji);
+//		mainMenu.getPlay().doClick();
+//		PlayMenuView playMenu = new PlayMenuView(this.kabasuji);
+//		playMenu.getPuzzle().doClick();
+//		AllLevelsView allLevelView = new AllLevelsView(this.kabasuji, PieceType.PUZZLE);
+//		allLevelView.getLevel1().doClick();
+//		LevelView lv = new LevelView(this.kabasuji, this.kabasuji.getLevel(PieceType.PUZZLE, 1));
+//		lv.getBack().doClick();
+//		allLevelView.getLevel2().doClick();
+//		LevelView lv2 = new LevelView(this.kabasuji, this.kabasuji.getLevel(PieceType.PUZZLE, 2));
+//		lv2.getBack().doClick();
+//		allLevelView.getBack().doClick();
+//		AllLevelsView allLevelView2 = new AllLevelsView(this.kabasuji, PieceType.LIGHTNING);
+//		allLevelView2.getLevel1().doClick();
 				
 	}
+	
+	public void testLightningLevel(){
+		MainMenuView mainMenu = new MainMenuView(this.kabasuji);
+		mainMenu.setVisible(true);
+		mainMenu.getPlay().doClick();
+		PlayMenuView playMenu = new PlayMenuView(this.kabasuji);
+		AllLevelsView allLevelView2 = new AllLevelsView(this.kabasuji, PieceType.LIGHTNING);
+		allLevelView2.getLevel1().doClick();
+		//assertTrue(allLevelView2.)
+	}
+	
+	
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
@@ -97,6 +109,8 @@ public class test extends TestCase {
 			e.printStackTrace();
 		}
 	}
+	
+	
 	
 	//boundary related:
 	
